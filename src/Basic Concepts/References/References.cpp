652c4dfe9e803327ba9966e536b7186fb349137d
KUTlime <kutlime@gmail.com>
2022-12-08 13:19:02 +0100

    Add projects for arrays and references

---
+++ src/Basic Concepts/References/References.cpp
@@ -0,0 +1,31 @@
+﻿// Demonstration of side effect of reference
+int& f(int& x)
+{
+	++x;
+	return x;
+}
+
+// That's why we often can see this modification.
+const int& g(const int& x)
+{
+	static int y = x;
+	++y;
+	return y;
+}
+
+int main()
+{
+	int i = 1;
+
+	printf("the value is %d\n", i);
+	printf("the value is %d\n", f(i));
+	printf("the value is %d\n", i); // Side effect of the reference. The is no compiler indication about the reference.
+	printf("the value is %d\n", g(i));
+
+	int temp = g(i);
+	temp++;
+
+	//int temp2 = (g(i)++); // Can do, why?
+
+
+}
