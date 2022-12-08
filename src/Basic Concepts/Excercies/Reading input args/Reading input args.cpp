d4b00bad6d8673eb8b40313891faeeafa79b73b8
KUTlime <kutlime@gmail.com>
2022-12-08 12:44:08 +0100

    Add excercise for reading input arguments

---
+++ src/Basic Concepts/Excercies/Reading input args/Reading input args.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main(int argCount, char ** args)
+{
+    std::cout << "Number of input parameter(s): " << argCount << std::endl;
+
+	for (size_t i = 0; i < argCount; i++)
+	{
+		std::cout << args[i] << std::endl;
+	}
+}
