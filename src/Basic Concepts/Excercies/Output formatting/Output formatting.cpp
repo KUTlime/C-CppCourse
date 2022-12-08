6f49590db4a0d5dcf2548620c42010e2f177a8c1
KUTlime <kutlime@gmail.com>
2022-12-08 12:43:50 +0100

    Add Output formatting project

---
+++ src/Basic Concepts/Excercies/Output formatting/Output formatting.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>​
+
+using namespace std; ​
+
+char * PrintBasicOperation(int x, int y, char operation)​
+{
+    char* buffer = (char*)malloc(100 * sizeof(char));
+    int result = 0; ​
+    switch (operation)​
+    { ​
+        case '+':​
+            result = x + y; ​
+            break; ​
+        case '-':​
+            result = x - y; ​
+            break; ​
+        case '/':​
+            result = x / y; ​
+            break; ​
+        case '*':​
+            result = x * y; ​
+            break; ​
+        case '%':​
+            result = x % y; ​
+            break; ​
+        case '<':​
+            result = x << y; ​
+            break; ​
+    }​
+
+    sprintf(buffer, "%d %c %d = %d\0", x, operation, y, result); ​
+
+    return buffer; ​
+
+}​;
+
+
+int main()​
+{
+        int x = 5;
+
+        int y = 7;
+
+        std::cout << PrintBasicOperation(x, y, '+') << "\t"
+
+        << PrintBasicOperation(x, y, '-') << "\t"
+
+        << PrintBasicOperation(x, y, '*') << "\t"
+
+        << PrintBasicOperation(y, x, '%') << "\t"
+
+        << PrintBasicOperation(x, y, '/') << endl;
+
+        printf("%s\t%s\t%s\t%s", PrintBasicOperation(x, y, '+'), PrintBasicOperation(x, y, '-'), PrintBasicOperation(x, y, '*'), PrintBasicOperation(x, y, '/')); ​
+            printf("The sizeof(%s) is %d", typeid(T).name(), sizeof(T));
+}
