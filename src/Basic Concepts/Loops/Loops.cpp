e9080ba2e382de78b6ad8515dcda7d85d501a96c
KUTlime <kutlime@gmail.com>
2022-12-08 12:44:32 +0100

    Add Loops project

---
+++ src/Basic Concepts/Loops/Loops.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+
+	while (true)
+	{
+
+	}
+	do
+	{
+
+	} while (true);
+	while (true)
+	{
+		break;
+	}
+	while (true)
+	{
+		// Some statement 
+		// Some statement 
+		// Some statement 
+		// Some statement 
+		continue;
+		// Some statement won't be executed
+	}
+	for (size_t i = 0; i < length; ++i)
+	{
+
+	}
+	auto array = { 1,2,3,4,5 };
+	for (auto& item : array)
+	{
+		std::cout << item << std::endl;
+	}
+    std::cout << "Hello World!\n";
+}
