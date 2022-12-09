fa76bdcc1ba482032e07cf462c65ebe58a535b76
KUTlime <kutlime@gmail.com>
2022-12-09 12:04:27 +0100

    Add passing value implementation

--- src/Basic Concepts/Functions/Passing values/Passing values.cpp
+++ src/Basic Concepts/Functions/Passing values/Passing values.cpp
@@ -1,20 +1,86 @@
-﻿// Passing values.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
-//
-
+﻿#include <cstdio>
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+// No input value
+void Func1()
+{
+	// Empty body
+}
+
+void Func2(int i)
+{
+	std::printf("Input value is: %i\n", i);
+	i = 0;
+	std::printf("Input value is: %i\n", i);
+}
+
+void Func3(int* i)
+{
+	std::printf("Input value is: %i\n", *i);
+	*i = 0;
+	std::printf("Input value is: %i\n", *i);
+}
+
+void Func4(int& i)
+{
+	std::printf("Input value is: %i\n", i);
+	i = 0;
+	std::printf("Input value is: %i\n", i);
+}
+
+// Immutable input parameter
+void Func5(const int& i)
+{
+	std::printf("Input value is: %i\n", i);
+	//i = 0; // compilation error
+	std::printf("Input value is: %i\n", i);
+}
+
+// Common use for immutable input
+void Func6(const string& str)
+{
+	printf("Input string is: %s\n", str.c_str());
+}
+
+void Func7()
+{
+	printf("Function 7\n");
+	static int i = 1;
+	printf("i is: %d\n", i);
+	++i;
+	printf("i is: %d\n", i);
+}
+
+void Func8()
+{
+	printf("Function 8\n");
+	int i = 0;
+	static int j = 3;
+	printf("i(non-static: %d, j(static) is: %d\n", i, j);
+	printf("i(non-static: %d, j(static) is: %d\n", ++i, ++j);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i = 1;
+	std::printf("Input value (main) is: %i\n", i);
+	Func2(i);
+	std::printf("Input value (main) is: %i\n", i);
+	Func3(&i);
+	std::printf("Input value (main) is: %i\n", i);
+	i = 2;
+	std::printf("Input value (main) is: %i\n", i);
+	Func4(i);
+	std::printf("Input value (main) is: %i\n", i);
+
+	Func7();
+	Func7();
+	Func8();
+	Func8();
+
+	std::cout << "Hello World!\n";
 }
 
-// Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
-// Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
-
-// Tipy pro zahájení práce:
-//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
-//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
-//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
-//   4. K zobrazení chyb použijte okno Seznam chyb.
-//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
-//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
